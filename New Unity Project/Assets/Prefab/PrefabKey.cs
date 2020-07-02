762d0cb15c229d9d301710935369f03976e712bc
EZEQUIEL LAUTARO HUAYTA ROMAY <44156990@comunidadort.edu.ar>
2020-07-01 22:57:53 -0300

    update

---
+++ New Unity Project/Assets/Prefab/PrefabKey.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabKey : MonoBehaviour
+{
+public AudioSource key;
+
+public string nombre;
+public Rigidbody rb;
+public static bool presionada = false;
+private void OnMouseDown() 
+{
+presionada=true;
+  transform.Rotate(-4,0,0);
+    rb.isKinematic=true;
+      key.Play();
+
+}    
+
+private void OnMouseUp() {
+  presionada=false;
+  key.Stop();
+  rb.isKinematic=false;
+}
+
+}
